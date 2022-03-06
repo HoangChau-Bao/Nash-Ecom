871137fcb5c7a7c89d7243fef1075dd21383de84
Hoang Chau <baohoang040200@gmail.com>
2022-03-06 19:13:54 +0700

    Add bootstrap template

---
+++ src/Rookie.Ecom.Customer/Pages/Category1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rookie.Ecom.Customer.Pages
+{
+    public class Category1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
