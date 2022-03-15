a32a33a752ea2019dfbe4d22c5b7be9d8b9f01fb
Hoang Chau <baohoang040200@gmail.com>
2022-03-15 18:15:08 +0700

    Add Identity Migration

--- src/Rookie.Ecom.Identity/Startup.cs
+++ src/Rookie.Ecom.Identity/Startup.cs
@@ -1,17 +1,41 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using Rookie.Ecom.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using System.Linq;
 
 namespace Rookie.Ecom.Identity
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public object Config { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = _config.GetConnectionString("DbConnection");
+
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseSqlServer(connectionString);
+
+            });
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowOrigins",
@@ -25,12 +49,32 @@ namespace Rookie.Ecom.Identity
 
             services.AddMvc();
 
+            var assembly = typeof(Startup).Assembly.GetName().Name;
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddTestUsers(InitData.GetUsers())
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(assembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(assembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.TokenCleanupInterval = 30;
+                })
+                //.AddAspNetIdentity<AppDbUser>()
                 .AddInMemoryIdentityResources(InitData.GetIdentityResources())
                 .AddInMemoryClients(InitData.GetClients());
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
